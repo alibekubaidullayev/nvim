2c44e95821d555ffb565614152b923fd22b295b9
Your Name <you@example.com>
2025-03-16 22:15:22 +0500

    asd

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,23 @@
+-- return {
+-- 	-- "slugbyte/lackluster.nvim",
+-- 	-- lazy = false,
+-- 	-- priority = 1000,
+-- 	-- init = function()
+-- 	-- 	-- vim.cmd.colorscheme("lackluster")
+-- 	-- 	-- vim.cmd.colorscheme("lackluster-night")
+-- 	-- 	-- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+-- 	-- 	vim.cmd.colorscheme("lackluster-mint")
+-- 	-- end,
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	priority = 1000,
+-- }
+
 return {
-	"slugbyte/lackluster.nvim",
-	lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	init = function()
-		-- vim.cmd.colorscheme("lackluster")
-		-- vim.cmd.colorscheme("lackluster-night")
-		-- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-		vim.cmd.colorscheme("lackluster-mint")
+	config = function()
+		vim.cmd.colorscheme("catppuccin-mocha") -- Set the colorscheme
 	end,
 }
